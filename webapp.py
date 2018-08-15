86d4a705098d7974700522b99656fbfeceb9c166
krzysztofsmokowski <krzysztofsmokowski@gmail.com>
2018-08-15 21:40:05 +0200

    minor fix in register method required

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://hg:project@localhost/hgdatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hgdatabase.db'
 db = SQLAlchemy(app)
 
 class Users(db.Model):
@@ -18,8 +18,9 @@ class Users(db.Model):
     username = db.Column(db.String(30))
     email = db.Column(db.String(100))
     password = db.Column(db.String(100))
-    register_date = db.Column(DateTime, default=datetime.datetime.utcnow) 
+    register_date = db.Column(DateTime, default=datetime.datetime.utcnow)
 
+db.create_all()
 Articles = Articles()
 @app.route('/')
 def index():
@@ -40,7 +41,7 @@ def article(id):
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=40)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validator.Length(min=6, max=50)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords do not match')])
@@ -50,8 +51,16 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == "POST" and form.validate():
-       return render_template('register.html', form=form) 
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        user = Users(name, email, username, password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now part of hellgate')
+        return redirect(url_for('index'))
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
